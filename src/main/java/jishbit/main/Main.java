a770e542f4f444e83cfced6638f1c2d98d22af2d
BuildTools <imperviousmc@gmail.com>
2016-09-14 23:40:00 -0400

    No more duplicate memes.

--- src/main/java/jishbit/main/Main.java
+++ src/main/java/jishbit/main/Main.java
@@ -1,5 +1,6 @@
 package jishbit.main;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.Random;
@@ -28,7 +29,14 @@ public class Main {
 	
 	public static IDiscordClient client;
 	public static IGuild guild;
-	
+
+	/**
+	 * List of submitted memes, do not add directly, use addMeme to retain max size
+	 */
+	private List<String> submittedMemes = new ArrayList<>();
+
+	private RestClient restClient;
+
 	String userAgent = "JishBit";
 	String clientID = "XAdloE1QjMnMyyjZjMtrhqao_1c";
 	String redirectURI = "www.google.ca";
@@ -38,6 +46,8 @@ public class Main {
 	}
 	
 	public Main() {
+		restClient = new HttpRestClient();
+		restClient.setUserAgent("JishBit");
 		connect();
 		client.getDispatcher().registerListener(this);
 	}
@@ -81,9 +91,6 @@ public class Main {
 	public void onMessageEvent(MessageReceivedEvent event) {
 		IMessage msg = event.getMessage();
 		String text = msg.getContent();
-		
-		RestClient restClient = new HttpRestClient();
-		restClient.setUserAgent("JishBit");
 
 		if(text.startsWith("`")) {
 			String cmd = text.substring(1).split(" ")[0].toLowerCase();
@@ -102,24 +109,20 @@ public class Main {
 			}
 			
 			if(cmd.equalsIgnoreCase("meme")) {
-				Random rand = new Random();
-				int n = rand.nextInt(5) + 1;
-				String subCode = Integer.toString(n);
-				Subs sub = Subs.getSubReddit(subCode);
-				String subToUse = sub.subreddit;
 
-				Submissions subms = new Submissions(restClient);
-				List<Submission> submissionsSubreddit = subms.ofSubreddit(subToUse, SubmissionSort.TOP, -1, 100, null, null, true);
-
-				int index = new Random().nextInt(submissionsSubreddit.size());
-				Submission submission = submissionsSubreddit.get(index);
+				Submission submission = findMeme(1);
+				//return if meme finding failed
+				if(submission == null){
+					sendMessage(event.getMessage().getChannel(), "Failed to find meme.");
+					return;
+				}
 
 				deleteMessage(msg);
 				String linkolio = submission.getUrl();
 				if(linkolio.contains("imgur.com") && !linkolio.matches(".+\\.[A-Za-z]{1,5}$") && !linkolio.contains("/a/") && !linkolio.contains("/gallery/")) {
 					linkolio += ".jpg";
 				}
-				sendMessage(msg.getChannel(), "*From /r/" + subToUse + ":* " + submission.getTitle() + "\n" + linkolio.replaceAll("&amp;", "&"));
+				sendMessage(msg.getChannel(), "*From /r/" + submission.getSubreddit() + ":* " + submission.getTitle() + "\n" + linkolio.replaceAll("&amp;", "&"));
 			}
 			
 			if(cmd.equalsIgnoreCase("list")) {
@@ -130,6 +133,50 @@ public class Main {
 		if(text.equalsIgnoreCase("<@222446374271057920>")) {
 			sendMessage(msg.getChannel(), "JishBit, the ultimate meme bot! \n Created by *Impervious* \n For a list of commands use `` `list``");
 		}
+	}
+
+	/**
+	 * Recursive function to find a random meme
+	 */
+	public Submission findMeme(int functionAttempt){
+		Random rand = new Random();
+		int n = rand.nextInt(5) + 1;
+		String subCode = Integer.toString(n);
+		Subs sub = Subs.getSubReddit(subCode);
+		String subToUse = sub.subreddit;
+
+		Submissions subms = new Submissions(restClient);
+
+		List<Submission> submissionsSubreddit = subms.ofSubreddit(subToUse, SubmissionSort.TOP, -1, 100, null, null, true);
+
+		Submission submissionToUse = null;
+		int attempts = 0;
+		while(submissionToUse == null || submittedMemes.contains(submissionToUse.getIdentifier())) {
+			int index = rand.nextInt(submissionsSubreddit.size());
+			submissionToUse = submissionsSubreddit.get(index);
+
+			//if after 50 attempts no unused meme is found in this subreddit, retry with new submission request and subreddit
+			attempts++;
+			if(attempts >= 50){
+				functionAttempt++;
+				//if after 10 recursive calls no meme is found, accept failure and return null
+				if(functionAttempt > 10) {
+					System.out.println("FAILED TO FIND MEME. MEMERGENCY.");
+					return null;
+				}else{
+					return findMeme(functionAttempt);
+				}
+			}
+		}
+		System.out.println("Found meme after " + functionAttempt + " subreddit searches and " + attempts + "posts.");
+		addMeme(submissionToUse.getIdentifier());
+		return submissionToUse;
+	}
+
+	public void addMeme(String postID){
+		//remove first element (earliest added meme) when list becomes too big
+		if(submittedMemes.size() >= 100) submittedMemes.remove(0);
+		submittedMemes.add(postID);
 	}
 
 	public static void sendMessage(IChannel channel, String message){
